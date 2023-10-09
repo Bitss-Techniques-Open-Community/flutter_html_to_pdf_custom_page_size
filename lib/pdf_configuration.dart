7b8c6d2e41dda335660e05a6154ce941e0177707
Siddharth Sharma <72195792+SiddByte@users.noreply.github.com>
2023-10-09 14:44:41 +0530

    Update pdf_configuration.dart

--- lib/pdf_configuration.dart
+++ lib/pdf_configuration.dart
@@ -3,7 +3,7 @@ class PdfConfiguration {
   double height=1000;
   int additionalConvertDelay=100;
 
-  PdfConfiguration({double width, double height, int additionalConvertDelay}) {
+  PdfConfiguration({double width=500, double height=1000, int additionalConvertDelay=100}) {
     this.width = width;
     this.height = height;
     this.additionalConvertDelay = additionalConvertDelay;
